2e418c3ee58254855ea5d70eae1268ed621f925e
Jeffyna Sharon J <146370395+jeffynasharon@users.noreply.github.com>
2026-03-11 18:02:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let text="Software Developer";
+
+let i=0;
+
+function typing(){
+
+if(i<text.length){
+
+document.querySelector(".typing").innerHTML+=text.charAt(i);
+
+i++;
+
+setTimeout(typing,100);
+
+}
+
+}
+
+typing();
